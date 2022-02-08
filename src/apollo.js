3e9a15aa8739d69eb5c5adeb6a647bbf471ba7dd
kimys-kor <ys2life1@naver.com>
2022-02-08 10:32:05 +0900

    finish authpage

--- src/apollo.js
+++ src/apollo.js
@@ -1,10 +1,33 @@
-module.exports = {
-  client: {
-    includes: ["./src/**/*.{tsx,ts}"],
-    tagName: "gql",
-    service: {
-      name: "instaclone-backend",
-      url: "http://localhost:4000/graphql",
-    },
-  },
+import { ApolloClient, InMemoryCache, makeVar } from "@apollo/client";
+
+const TOKEN = "TOKEN";
+const DARK_MODE = "DARK_MODE";
+
+export const isLoggedInVar = makeVar(Boolean(localStorage.getItem(TOKEN)));
+
+export const logUserIn = (token) => {
+  localStorage.setItem(TOKEN, token);
+  isLoggedInVar(true);
 };
+
+export const logUserOut = () => {
+  localStorage.removeItem(TOKEN);
+  window.location.reload();
+};
+
+export const darkModeVar = makeVar(Boolean(localStorage.getItem(DARK_MODE)));
+
+export const enableDarkMode = () => {
+  localStorage.setItem(DARK_MODE, "enabled");
+  darkModeVar(true);
+};
+
+export const disableDarkMode = () => {
+  localStorage.removeItem(DARK_MODE);
+  darkModeVar(false);
+};
+
+export const client = new ApolloClient({
+  uri: "http://localhost:4000/graphql",
+  cache: new InMemoryCache(),
+});
