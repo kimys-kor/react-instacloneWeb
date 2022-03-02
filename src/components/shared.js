24eb87fd9e330679fc0d22e033a37344c9e1d762
kimys-kor <ys2life1@naver.com>
2022-03-02 14:56:23 +0900

    edit photo component

--- src/components/shared.js
+++ src/components/shared.js
@@ -1,5 +1,4 @@
 import styled from "styled-components";
-
 export const BaseBox = styled.div`
   background-color: ${(props) => props.theme.bgColor};
   border: 1px solid ${(props) => props.theme.borderColor};
@@ -8,4 +7,8 @@ export const BaseBox = styled.div`
 export const FatLink = styled.span`
   font-weight: 600;
   color: rgb(142, 142, 142);
+`;
+
+export const FatText = styled.span`
+  font-weight: 600;
 `;
--- src/hooks/useUser.js
+++ src/hooks/useUser.js
@@ -15,7 +15,6 @@ function useUser() {
   const { data } = useQuery(ME_QUERY, {
     skip: !hasToken,
   });
-  console.log(data);
   useEffect(() => {
     if (data?.me === null) {
       logUserOut();
